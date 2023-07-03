7aa0c317930fd4039441f2850c6c4954c95fa6f8
alebastrov <alebastrov@mailinator.com>
2023-07-03 11:57:51 +0300

    added raw response ws method

--- src/main/java/org/example/webservice/MainWS.java
+++ src/main/java/org/example/webservice/MainWS.java
@@ -1,6 +1,9 @@
 package org.example.webservice;
 
 import lombok.ToString;
+import oap.http.Cookie;
+import oap.http.Http;
+import oap.util.Dates;
 import oap.util.Lists;
 import oap.ws.Response;
 import oap.ws.WsMethod;
@@ -9,7 +12,10 @@ import oap.ws.validate.WsValidateJson;
 import org.example.NotNull;
 import org.example.linking.MessageProducer;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import static oap.http.server.nio.HttpServerExchange.HttpMethod.GET;
 import static oap.http.server.nio.HttpServerExchange.HttpMethod.POST;
@@ -36,6 +42,24 @@ public class MainWS {
     @WsMethod( path = "/substitute" )
     public String getRealValue() {
         return "real value is: " + producer.toBeResolved.value + ", should be 2";
+    }
+
+    @WsMethod( path = "/raw1", raw = true, description = "this method returns a file with name 'raw1.tsw'" )
+    public Response getRawResponse() {
+        Consumer<OutputStream> body = outputStream -> {
+            try {
+                outputStream.write("This\tis\ta\traw\tresponse\tdemo".getBytes());
+                outputStream.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        return Response.ok()
+                .withStatusCode(Http.StatusCode.ACCEPTED)
+                .withBody(body, false)
+                .withContentType(Http.ContentType.TEXT_TSV)
+                .withCookie(new Cookie( "X_RAY", "palm" ))
+                .withHeader("x-file-date", Dates.nowUtcDate().toString());
     }
 
     @WsMethod( method = POST, path = "/addData", produces = "text/plain" )
